48f9c8d15d161e01275e85b780d0099b3312eea0
Song <503027010@logon.ds.ge.com>
2018-10-16 18:04:42 +0800

    import code from repo SourceCode

---
+++ C#/ConsoleApps/ConsoleApp/Program0.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program0
+    {
+        //test: force push after merge PR
+        static void Main0(string[] args)
+        {
+            Console.ReadLine();
+            TestValueTypeandReferenceType test = new TestValueTypeandReferenceType();
+            test.Test();
+
+            TestBoxingandUnboxing test2 = new TestBoxingandUnboxing();
+            test2.test();
+
+        }
+    }
+}
---
+++ C#/WinForm/Lab/Temp/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Temp
+{
+
+    public class Person
+    {
+        public static int stint;
+        public const int coint = 3;
+        public readonly int reint;
+
+        public string name;
+
+        public Person(string p)
+        {
+            reint = 9;
+            this.name = p;
+
+        }
+        public Person()
+        {
+            reint = 9;
+            stint = 8;
+            reint = 19;
+
+            this.name = "no one";
+
+        }
+
+        public void count()
+        { 
+            stint = 10;
+
+            int cnt = 0;
+            Console.WriteLine(cnt);
+        }
+    }
+}
