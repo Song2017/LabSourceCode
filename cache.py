5a5ca5336379d805742f457e5f5d841da81ffc76
Song <503027010@logon.ds.ge.com>
2018-10-24 19:57:15 +0800

    python - 13 web api

---
+++ cache.py
@@ -0,0 +1,16 @@
+if (VKCon.SelectedEquipType != EquipType.CV && VKCon.SelectedEquipType != EquipType.RV)
+{
+"EDATE"
+"EDATE1"
+}
+
+if (VKCon.SelectedEquipType == EquipType.RV)
+{
+"EXTRADATE1 
+"EXTRADATE2"
+}
+if (VKCon.SelectedEquipType == EquipType.CV)
+{
+"EXTRADATE"
+"EXTRADATE1"
+}
