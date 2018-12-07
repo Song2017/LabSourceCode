35f8359eccb3f3d90f7944c22598aab041b97c4f
Song <503027010@logon.ds.ge.com>
2018-12-07 16:11:03 +0800

    python translate with excel

--- Python/Exercise_2017/baiduTranslateWithExcel.py
+++ Python/Exercise_2017/baiduTranslateWithExcel.py
@@ -1,65 +1,59 @@
-#/usr/bin/env python
-#coding=utf8
- 
-import hashlib 
+# /usr/bin/env python
+# coding=utf8
+
+import hashlib
 import random
 import openpyxl
 from openpyxl import Workbook
-import json
-import urllib.request
+import requests
 
-print('translate begin...')
-
-#set parameter
+# set baidu develop parameter
+apiurl = 'http://api.fanyi.baidu.com/api/trans/vip/translate'
 appid = '20180816000194959'
 secretKey = 'lWzwUiWmhRORknf68FCT'
-fromLang = 'en' # 源语言
-toLang = 'zh'   # 翻译后的语言
-srcFilename = 'c:\_Work\chinesetranslation.xlsx'
-srcColumn=2
-srcRowBegin=1
-srcRowEnd=28
-srcSheet = 'Sheet2'
-desFilename = 'c:\_Work\chinesetranslation_result.xlsx'
-desColumn=1
-desSheet = 'result2'
 
-
-def translateBaidu(content): 
-    apiurl = 'http://api.fanyi.baidu.com/api/trans/vip/translate'
-    q = content 
-    salt = random.randint(32768, 65536)
-    sign = str(appid) + q + str(salt) + secretKey 
+# 翻译内容 源语言 翻译后的语言
+def translateBaidu(content, fromLang='en', toLang='zh'):
+    salt = str(random.randint(32768, 65536))
+    sign = appid + content + salt + secretKey
     sign = hashlib.md5(sign.encode("utf-8")).hexdigest()
-    myurl = apiurl + '?appid=' + appid + '&q=' + urllib.parse.quote(q) + '&from=' + fromLang + '&to=' + toLang + '&salt=' + str(salt) + '&sign=' + sign
- 
-    try: 
+
+    try:
         paramas = {
-            'q':q,
-            'from':'zh',
-            'to':'en',
-            'appid':'%s'%appid,
-            'salt':'%s'%salt,
-            'sign':'%s'%sign
-            } 
-        response = urllib.request.urlopen(myurl)
-        jsonResponse = response.read()# 获得返回的结果，结果为json格式
-        js = json.loads(jsonResponse)  # 将json格式的结果转换字典结构
-        dst = str(js["trans_result"][0]["dst"])  # 取得翻译后的文本结果 
+            'appid': appid,
+            'q': content,
+            'from': fromLang,
+            'to': toLang,
+            'salt': salt,
+            'sign': sign
+        }
+        response = requests.get(apiurl, paramas)
+        jsonResponse = response.json()  # 获得返回的结果，结果为json格式
+        dst = str(jsonResponse["trans_result"]
+                  [0]["dst"])  # 取得翻译后的文本结果
         return dst
     except Exception as e:
-        print(e) 
+        print(e)
 
 
-wb = openpyxl.load_workbook(srcFilename)
-ws = wb[srcSheet]
-wb2 = Workbook()
-ws2 = wb2.create_sheet(title=desSheet)
+def excelTrans(srcFilename=r'c:\_Work\source.xlsx', desFilename=r'c:\_Work\result.xlsx'
+               srcSheet='Sheet2',  srcColumn=1, srcRowBegin=1, srcRowEnd=28, 
+               desColumn=1, desSheet='result2'):
+    wb = openpyxl.load_workbook(srcFilename)
+    ws = wb[srcSheet]
+    wb2 = Workbook()
+    ws2 = wb2.create_sheet(title=desSheet)
 
-for i in range (srcRowBegin,srcRowEnd,1):
-    result=ws.cell(row=i, column=srcColumn).value
-    if not (result is None):
-        ws2.cell(row=i-srcRowBegin+1,column=desColumn).value = translateBaidu(result)
+    for i in range(srcRowBegin, srcRowEnd, 1):
+        result = ws.cell(row=i, column=srcColumn).value
+        if not (result is None):
+            ws2.cell(row=i-srcRowBegin+1,
+                     column=desColumn).value = translateBaidu(result)
 
-wb2.save(desFilename)
-print('ending...') 
+    wb2.save(desFilename)
+
+
+if __name__ == '__main__':
+    print('translate begin...')
+    excelTrans()
+    print('ending...')
