c6e08970f0233dc528709ac8c92aa589a0dafc99
Song <503027010@logon.ds.ge.com>
2019-07-02 17:04:04 +0800

    prune

--- Python/Exercise_2017/checkvoter.py
+++ Python/Exercise_2017/checkvoter.py
@@ -1,11 +1,13 @@
 voted = {}
 
-def check_voter(name) :
-    if voted.get(name) :
+
+def check_voter(name):
+    if voted.get(name):
         print('kick out')
-    else :
+    else:
         voted[name] = True
         print(name + ' voted')
+
 
 check_voter('alice')
 check_voter('han')
--- Python/Exercise_2017/primenum.py
+++ Python/Exercise_2017/primenum.py
@@ -5,13 +5,13 @@ num = int(input('enter a number : '))
 
 floor = int(num//2 + 1)
 
-if num<= 1:
-	print('prime num is greater than 1')
+if num <= 1:
+    print('prime num is greater than 1')
 else:
-	for i in range(2, floor):
-		if (num % i) == 0:
-			print(num, "is not a prime number")
-			print(i, " times ", num/i, " is ", num)
-			break
-		else:
-			print(num, " is a prime number") 
+    for i in range(2, floor):
+        if (num % i) == 0:
+            print(num, "is not a prime number")
+            print(i, " times ", num/i, " is ", num)
+            break
+        else:
+            print(num, " is a prime number")
