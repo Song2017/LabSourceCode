3907a5dff08784c18c830f1d0f00af52f698f3f2
ben.song <ben.song@samarkand.global>
2020-06-01 11:53:28 +0800

    update go learning

--- GO/src/puzzlers/article2/q1/demo2.go
+++ GO/src/puzzlers/article2/q1/demo2.go
@@ -5,13 +5,15 @@ import (
 	"fmt"
 )
 
-var name string
+// var name string
 
 func init() {
-	flag.StringVar(&name, "name", "everyone", "The greeting object.")
+	// flag.StringVar(&name, "name", "everyone", "The greeting object.")
+	
 }
 
 func main() {
+	var name = flag.String("name", "everyone", "The greeting object.")
 	flag.Parse()
-	fmt.Printf("Hello, %s!\n", name)
+	fmt.Printf("Hello, %s!\n", *name)
 }
