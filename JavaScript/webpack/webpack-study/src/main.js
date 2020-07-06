45e6df23aabf148855c923d564bb7ce27a515f76
ben.song <ben.song@samarkand.global>
2020-07-06 10:38:28 +0800

    zip js source code

--- JavaScript/webpack/webpack-study/src/main.js
+++
@@ -1,7 +0,0 @@
-import $ from 'jquery'
-import './css/index.css'
-
-$(function () {
-    $('li:odd').css('backgroundColor', 'orange')
-    $('li:even').css('backgroundColor', function () { return '#' + 'D97634' })
-})
